988637dce6e28bc8fb7fa8dc94d334c7a789ef93
Hugo Gomes <gomes.hugo00@gmail.com>
2023-01-29 00:37:33 -0300

    feat: add authentication in book routers

--- src/router/book-router.ts
+++ src/router/book-router.ts
@@ -10,9 +10,11 @@ import {
 import validateBook from "../middleware/validate-book-middleware.js";
 import validateUpdatedBook from "../middleware/validate-updatedbook-middleware.js";
 import validateParams from "../middleware/validate-params-middleware.js";
+import authentication from "../middleware/authentication-middleware.js";
 
 const bookRouter = Router();
 
+bookRouter.all("/*", authentication);
 bookRouter.get("/book/all", getAllBook);
 bookRouter.get("/book/done", howManyBooksWereDone);
 bookRouter.get("/book/:id", validateParams, getBook);
